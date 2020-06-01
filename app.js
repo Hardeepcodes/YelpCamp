127abe19f9b689c83412fcf05ea889434b3acc29
hardeepcodes <45474579+Hardeepcodes@users.noreply.github.com>
2020-05-31 21:25:50 -0700

    Add feature of adding Comments.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express"),
     bodyParser= require("body-parser"),
 	mongoose = require("mongoose"),
 	Campground = require("./models/campground"),
+	Comment = require("./models/comment"),
 	seedDb = require("./seeds")
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true});	
@@ -10,20 +11,6 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 
 seedDb();
-
-// Campground.create({
-
-// 		name: "Granite Hill", 
-// 		image: "https://www.reserveamerica.com/webphotos/racms/articles/images/fef91bb3-1dff-444d-b0e5-d14db129ce1d_image2_0-main-tent.jpg",
-// 		description: "Beautiful Granite Hill"
-
-// 	}, function(err, campground){
-// 		if(err){
-// 			console.log(err);
-// 		} else{
-// 			console.log(campground);
-// 		}
-// 	});
 
 app.get("/", function(req, res){
 	res.render("landing");
@@ -34,7 +21,7 @@ app.get("/campgrounds", function(req, res){
 		if(err){
 			console.log(err);
 		} else{
-			res.render("index", {campgrounds: allCampgrounds});
+			res.render("campgrounds/index", {campgrounds: allCampgrounds});
 		}
 	})
 });
@@ -55,7 +42,7 @@ app.post("/campgrounds", function(req, res){
 });
 
 app.get("/campgrounds/new", function(req, res){
-	res.render("new");
+	res.render("campgrounds/new");
 });
 
 app.get("/campgrounds/:id", function(req, res){
@@ -63,11 +50,39 @@ app.get("/campgrounds/:id", function(req, res){
 		if(err){
 			console.log(err);
 		} else {
-			res.render("show", {campground: foundCampground});
+			res.render("campgrounds/show", {campground: foundCampground});
 		}
 	});
 });
 
+app.get("/campgrounds/:id/comments/new", function(req, res){
+	Campground.findById(req.params.id, function(err, campground){
+		if(err){
+			console.log(err);
+		} else {
+			res.render("comments/new", {campground: campground});
+		}
+	});
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+	Campground.findById(req.params.id, function(err, campground){
+		if(err){
+			console.log(err);
+		} else {
+			Comment.create(req.body.comment, function(err, comment){
+				if(err){
+					console.log(err);
+				} else {
+					campground.comments.push(comment);
+					campground.save();
+					res.redirect("/campgrounds/" + campground._id);
+				}
+			});
+		}
+	});
+});
+	
 app.listen(3000, function(){
 	console.log("YelpCamp has started!!");
 });
