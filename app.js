f1832b189f5b6c193ea93d38d4d170d815b219dc
hardeepcodes <45474579+Hardeepcodes@users.noreply.github.com>
2020-06-05 13:53:03 -0700

    User Association to Adding Comments.

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-seedDb();
+// seedDb();
 
 app.use(require("express-session")({
 	secret: "I Love C0ding",
